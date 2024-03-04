96cf4e20a783df55bafe9f6aef46591c66a8b354
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2024-03-04 12:53:31 -0600

    wifi

--- ESP32C3/main/main.c
+++ ESP32C3/main/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/uart.h"
@@ -36,16 +38,18 @@ void _UPDI_error_check_failed(const char *file, int line, const char *function, 
         }                                                   \
     } while(0)
 
+uart_port_t UPDI_uart_num;
+
 //read UPDI byte
 bool UPDI_Read(uint8_t *val)
 {
-    return uart_read_bytes(1, val, 1, pdMS_TO_TICKS(100)) > 0;
+    return uart_read_bytes(UPDI_uart_num, val, 1, pdMS_TO_TICKS(100)) > 0;
 }
 
 //write UPDI byte
 bool UPDI_Write(uint8_t val)
 {
-    return uart_write_bytes(1, &val, 1) > 0;
+    return uart_write_bytes(UPDI_uart_num, &val, 1) > 0;
 }
 
 //UPDI send Break
@@ -53,13 +57,13 @@ void UPDI_Break()
 {
     uint32_t baudrate = 0;
 
-    uart_wait_tx_done(1, pdMS_TO_TICKS(30)); //wait for all bytes to be flushed
-    uart_get_baudrate(1, &baudrate);
-    uart_set_baudrate(1, 10000);
+    uart_wait_tx_done(UPDI_uart_num, pdMS_TO_TICKS(30)); //wait for all bytes to be flushed
+    uart_get_baudrate(UPDI_uart_num, &baudrate);
+    uart_set_baudrate(UPDI_uart_num, 10000);
     UPDI_Write(0);  // send a zero byte
-    uart_wait_tx_done(1, pdMS_TO_TICKS(30)); // wait for 0 byte to finish before restore normal baudrate
-    uart_flush_input(1);
-    uart_set_baudrate(1, baudrate); // set baudrate back to normal after break is sent
+    uart_wait_tx_done(UPDI_uart_num, pdMS_TO_TICKS(30)); // wait for 0 byte to finish before restore normal baudrate
+    uart_flush_input(UPDI_uart_num);
+    uart_set_baudrate(UPDI_uart_num, baudrate); // set baudrate back to normal after break is sent
 }
 
 void UPDI_Idle()
@@ -104,89 +108,22 @@ bool UPDI_EnableAck()
     return true;
 }
 
-static void echo_jtag_to_uart_task(void *arg)
-{
-    uint8_t *data = (uint8_t *) malloc(1024);
-    while (1) {
-        // Read data from the UART
-        int len = usb_serial_jtag_read_bytes(data, 1024, pdMS_TO_TICKS(100));
-        // Write data back to the JTAG
-        if (len) {
-            uart_write_bytes(0, data, len);
-        }
-    }
-}
-
-static void echo_task(void *arg)
-{
-    //Setup W806 UART
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT,
-    };
-
-    ESP_ERROR_CHECK(uart_driver_install(0, 1024, 0, 0, NULL, 0));
-    ESP_ERROR_CHECK(uart_set_rx_full_threshold(0, 1));
-    ESP_ERROR_CHECK(uart_param_config(0, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(0, W806_UART_RX_PIN, W806_UART_TX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    
-    //Setup Serial JTAG
-    usb_serial_jtag_driver_config_t usb_serial_jtag_config = {
-        .tx_buffer_size = 1024,
-        .rx_buffer_size = 1024,
-    };
-    ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&usb_serial_jtag_config));
-
-    //Setup JTAG to Serial echo
-    xTaskCreate(echo_jtag_to_uart_task, "echo_jtag_to_uart_task", 2048, NULL, 10, NULL);
-
-    //Release W806 reset
-    vTaskDelay(pdMS_TO_TICKS(10));
-    gpio_config_t io_conf = {};
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = (1ULL<<W806_RESET_PIN) | (1ULL<<W806_BOOT_PIN);
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    ESP_ERROR_CHECK(gpio_config(&io_conf));
-    ESP_ERROR_CHECK(gpio_set_level(W806_RESET_PIN, 0));
-    ESP_ERROR_CHECK(gpio_set_level(W806_BOOT_PIN, 1));
-    vTaskDelay(pdMS_TO_TICKS(10));
-    ESP_ERROR_CHECK(gpio_set_level(W806_RESET_PIN, 1));
-
-    uint8_t *data = (uint8_t *) malloc(1024);
-    while (1) {
-        // Read data from the UART
-        int len = uart_read_bytes(0, data, 1024, pdMS_TO_TICKS(100));
-        // Write data back to the JTAG
-        if (len) {
-            usb_serial_jtag_write_bytes(data, len, pdMS_TO_TICKS(100));
-        }
-    }
-}
-
-extern esp_err_t gpio_od_enable(gpio_num_t gpio_num);
-
-bool UPDI_Enable(gpio_num_t pin)
+bool UPDI_Enable(uart_port_t uart_num, gpio_num_t tx_pin, gpio_num_t rx_pin)
 {
     //start UPDI
-    gpio_set_level(pin, 1);
-    gpio_set_direction(pin, GPIO_MODE_OUTPUT);
-    gpio_hal_iomux_func_sel(GPIO_PIN_MUX_REG[pin], PIN_FUNC_GPIO);
-    gpio_set_direction(pin+1, GPIO_MODE_INPUT);
-    gpio_hal_iomux_func_sel(GPIO_PIN_MUX_REG[pin+1], PIN_FUNC_GPIO);
+    gpio_set_level(tx_pin, 1);
+    gpio_set_direction(tx_pin, GPIO_MODE_OUTPUT);
+    gpio_hal_iomux_func_sel(GPIO_PIN_MUX_REG[tx_pin], PIN_FUNC_GPIO);
+    gpio_set_direction(rx_pin, GPIO_MODE_INPUT);
+    gpio_hal_iomux_func_sel(GPIO_PIN_MUX_REG[rx_pin], PIN_FUNC_GPIO);
 
     //send enable pulse
-    gpio_set_level(pin, 0);
-    gpio_set_level(pin, 1);
+    gpio_set_level(tx_pin, 0);
+    gpio_set_level(tx_pin, 1);
 
     // //wait for enable pulse end
     uint32_t i = 0;
-    while(!gpio_get_level(pin+1) && i++ < 15) esp_rom_delay_us(100);
+    while(!gpio_get_level(rx_pin) && i++ < 15) esp_rom_delay_us(100);
 
     // Setup UPDI UART
     uart_config_t uart_config = {
@@ -198,12 +135,14 @@ bool UPDI_Enable(gpio_num_t pin)
         .source_clk = UART_SCLK_DEFAULT,
     };
 
-    ESP_ERROR_CHECK(uart_driver_install(1, 1024, 0, 0, NULL, 0));
-    ESP_ERROR_CHECK(uart_set_rx_full_threshold(1, 1));
-    ESP_ERROR_CHECK(uart_param_config(1, &uart_config));
-    ESP_ERROR_CHECK(uart_set_mode(1, UART_MODE_RS485_HALF_DUPLEX));
-    ESP_ERROR_CHECK(uart_set_pin(1, pin, pin+1, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    gpio_set_pull_mode(pin+1, GPIO_FLOATING);
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, 1024, 0, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_set_rx_full_threshold(uart_num, 1));
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    ESP_ERROR_CHECK(uart_set_mode(uart_num, UART_MODE_RS485_HALF_DUPLEX));
+    ESP_ERROR_CHECK(uart_set_pin(uart_num, tx_pin, rx_pin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    gpio_set_pull_mode(rx_pin, GPIO_FLOATING);
+
+    UPDI_uart_num = uart_num;
 
     //send NVMPROG Key to enable UPDI
     uint64_t key = UPDI_KEYNVMPROG;
@@ -362,10 +301,10 @@ bool UPDI_WriteFlashOrEEPROM(uint32_t address, uint8_t *data, uint32_t length)
     return true;
 }
 
-bool UPDI_Program(gpio_num_t pin, uint8_t *data, uint32_t length)
+bool UPDI_Program(uart_port_t uart_num, gpio_num_t tx_pin, gpio_num_t rx_pin, uint8_t *data, uint32_t length)
 {
     ESP_LOGI("UPDI", "programming attiny");
-    UPDI_ERROR_CHECK(UPDI_Enable(pin));
+    UPDI_ERROR_CHECK(UPDI_Enable(uart_num, tx_pin, rx_pin));
     if(!UPDI_EraseChip()) UPDI_ERROR_CHECK(UPDI_EraseChip());
     if(!UPDI_NVMProg()) UPDI_ERROR_CHECK(UPDI_NVMProg());
     UPDI_ERROR_CHECK(UPDI_WriteFlashOrEEPROM(0x8000, data, length));
@@ -374,13 +313,135 @@ bool UPDI_Program(gpio_num_t pin, uint8_t *data, uint32_t length)
     return true;
 }
 
-void app_main(void)
+static void echo_jtag_to_uart_task(void *arg)
 {
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    size_t attinyload_bytes = attinyload_end - attinyload_start;
-    uint8_t *attinyload = (uint8_t *)calloc(attinyload_bytes, sizeof(uint8_t));
-    memcpy(attinyload, attinyload_start, attinyload_bytes);
-    UPDI_Program(4, attinyload, attinyload_bytes);
+    uint8_t *data = (uint8_t *) malloc(1024);
+    while (1) {
+        // Read data from the UART
+        int len = usb_serial_jtag_read_bytes(data, 1024, pdMS_TO_TICKS(100));
+        // Write data back to the JTAG
+        if (len) {
+            uart_write_bytes(0, data, len);
+        }
+    }
+}
+
+static void echo_task(void *arg)
+{
+    //Setup W806 UART
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+
+    ESP_ERROR_CHECK(uart_driver_install(0, 1024, 0, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_set_rx_full_threshold(0, 1));
+    ESP_ERROR_CHECK(uart_param_config(0, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(0, W806_UART_RX_PIN, W806_UART_TX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    
+    //Setup Serial JTAG
+    usb_serial_jtag_driver_config_t usb_serial_jtag_config = {
+        .tx_buffer_size = 1024,
+        .rx_buffer_size = 1024,
+    };
+    ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&usb_serial_jtag_config));
+
+    //Setup JTAG to Serial echo
+    xTaskCreate(echo_jtag_to_uart_task, "echo_jtag_to_uart_task", 2048, NULL, 10, NULL);
+
+    //Release W806 reset
+    vTaskDelay(pdMS_TO_TICKS(10));
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = (1ULL<<W806_RESET_PIN) | (1ULL<<W806_BOOT_PIN);
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+    ESP_ERROR_CHECK(gpio_set_level(W806_RESET_PIN, 0));
+    ESP_ERROR_CHECK(gpio_set_level(W806_BOOT_PIN, 1));
+    vTaskDelay(pdMS_TO_TICKS(10));
+    ESP_ERROR_CHECK(gpio_set_level(W806_RESET_PIN, 1));
+
+    uint8_t *data = (uint8_t *) malloc(1024);
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(0, data, 1024, pdMS_TO_TICKS(100));
+        // Write data back to the JTAG
+        if (len) {
+            usb_serial_jtag_write_bytes(data, len, pdMS_TO_TICKS(100));
+        }
+    }
+}
+
+void wifi_init_softap()
+{
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = "EFIGenie",
+            .ssid_len = strlen("EFIGenie"),
+            // .password = EXAMPLE_ESP_WIFI_PASS,
+            .authmode = WIFI_AUTH_WPA2_PSK,
+            .pmf_cfg = { .required = true },
+        },
+    };
+    if (strlen((char *)wifi_config.ap.password) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+typedef struct 
+{
+    uart_port_t uart_num
+    gpio_num_t tx_pin;
+    gpio_num_t rx_pin;
+
+} telenet_uart_config_t;
+
+static void telenet_uart(void * arg)
+{
+
+}
+
+void app_main()
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_softap();
+
+    telenet_uart_config_t telnet_uart_config = 
+    {
+
+    };
+
+    xTaskCreate(telenet_uart, "telnet", 4096, &telnet_uart_config, 5, NULL);
+
+    // vTaskDelay(pdMS_TO_TICKS(1000));
+    // size_t attinyload_bytes = attinyload_end - attinyload_start;
+    // uint8_t *attinyload = (uint8_t *)calloc(attinyload_bytes, sizeof(uint8_t));
+    // memcpy(attinyload, attinyload_start, attinyload_bytes);
+    // UPDI_Program(4, attinyload, attinyload_bytes);
     
     // xTaskCreate(echo_task, "echo_task", 2048, NULL, 10, NULL);
 }
